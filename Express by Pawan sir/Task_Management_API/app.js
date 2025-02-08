3302e1fde956597fca068538e0fe388097a17d77
Vikas92-dot <vikasmathe143@gmail.com>
2025-02-08 17:35:15 +0530

    Applying validation and decrypt password using bcryptjs

--- Express by Pawan sir/Task_Management_API/app.js
+++ Express by Pawan sir/Task_Management_API/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import AdminRouter from './routes/admin.route.js';
+import TaskRouter from './routes/task_priority.route.js';
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/admin',AdminRouter);
+app.use('/task_priority',TaskRouter);
 
 app.listen(3000,()=>{
     console.log("Server started at http://localhost:3000");
